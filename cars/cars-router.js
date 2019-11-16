3db78786b2eb93dc4199ec65d93b32236bb308fc
Mada Gower <gmgower@gmail.com>
2019-11-15 22:48:44 -0600

    PUT & DELETE.

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -9,11 +9,18 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     const carData = req.body;
+    // console.log("TCL: carData", carData)
 
-    db('cars').insert(carData)
+    db('cars').insert(carData)    
     .then(ids => {
+    // console.log("TCL: ids", ids)
+        
         db('cars').where({id: ids[0]})
+        // console.log("TCL: {id: ids[0]}", {id: ids[0]})
+        
         .then(newCarEntry => {
+        console.log("TCL: newCarEntry", newCarEntry)
+            
             res.status(201).json(newCarEntry);
         });
     })
@@ -47,8 +54,55 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
 
+    db('cars').where({id}).update(changes)
+    .then(carObjId => {
+        if(carObjId) {
+            db('cars')
+            .where({id})
+            .first()
+            .then(carUpdateObj => {
+                res.status(200).json(carUpdateObj)
+            })
+        } else {
+            res.status(404).json({message: 'invalid id'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Problem with database.'})
+    })
+ })
 
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+
+    db('cars')
+    .where({id})
+    .del()
+    .then(carObjId => {
+    console.log("TCL: carObjId", carObjId)
+        // if(carObjId) {
+        // db('cars')
+        // .where({id})  
+        // .first()      
+        // .then(carDeleteObj => {
+        // console.log("TCL: carDeleteObj", carDeleteObj)
+            
+        // })
+        // } else {
+        //     res.status(404).json({message: 'invalid id.'})
+        // }
+        carObjId 
+        ? res.status(200).json({deleted: carObjId})
+        : res.status(404).json({message: 'Invalid ID'})        
+    })
+    .catch(err => {
+        res.status(500).json({message: "Problem with the database."})
+    })
+})
 
 // ? s13a
 module.exports = router
